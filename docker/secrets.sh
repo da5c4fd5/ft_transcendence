45863b095fb5d7cf9b0fc8ced8a4eb6684c8f50e
da5c4fd5 <272876439+da5c4fd5@users.noreply.github.com>
2026-04-14 20:05:52 +0200

    feat: docker pipeline using Makefile wizardry

---
+++ docker/secrets.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+
+mkdir -p docker/secrets
+umask 077
+[ -f docker/secrets/postgres_password ] || openssl rand -hex 32 > docker/secrets/postgres_password
+[ -f docker/secrets/jwt_secret ] || openssl rand -hex 48 > docker/secrets/jwt_secret
+[ -f docker/secrets/powerdnsadmin_api_key ] || : > docker/secrets/powerdnsadmin_api_key
